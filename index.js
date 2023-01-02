7378c6abd210be06f242d7c14b6887ca457cee54
afruzaakter <afruzaaktersonia1995@gmail.com>
2023-01-02 11:29:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) =>{
+    res.send('Server is Running')
+});
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
