b9488fe7be55be79c074bc67a7f110574857d314
afruzaakter <afruzaaktersonia1995@gmail.com>
2023-02-14 15:03:37 +0600

    update code

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.feqszmo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJWT(req, res,next){
-  const authHeader= req.headers.authorization;
-  if(!authHeader){
-    return res.status(401).send({message: 'UnAuthorized Access'})
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized Access' })
   }
-  const token=authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN , function(err, decoded) {
-    if(err){
-      return res.status(403).send({message: 'Forbidden Access'})
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden Access' })
     }
-    req.decoded=decoded;
+    req.decoded = decoded;
     next();
   });
 }
@@ -42,30 +42,31 @@ async function run() {
     const departmentCollection = client.db("store_management").collection("department");
     const designationCollection = client.db("store_management").collection("designation");
     const productKeyCollection = client.db("store_management").collection("productKey");
-    const budgetCodeCollection= client.db("store_management").collection("budgetcode");
-    const productCollection= client.db("store_management").collection("product");
-    const supplierCollection= client.db("store_management").collection("supplier");
-    const stockAdjustCollection= client.db("store_management").collection("stockadjust");
-    const allUsersCollection= client.db("store_management").collection("allUsers");
-    
+    const budgetCodeCollection = client.db("store_management").collection("budgetcode");
+    const productCollection = client.db("store_management").collection("product");
+    const supplierCollection = client.db("store_management").collection("supplier");
+    const stockAdjustCollection = client.db("store_management").collection("stockadjust");
+    const allUsersCollection = client.db("store_management").collection("allUsers");
+
+
     // ====================== // All User start \\===================
 
-    app.put('/allUsers/:email', async(req,res)=>{
-      const email=req.params.email;
-      const user=req.body;
-      const filter={email:email};
-      const options={upsert: true};
-      const updateDoc={
-        $set:user,
+    app.put('/allUsers/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
       };
-      const result= await allUsersCollection.updateOne(filter,updateDoc,options);
-      const token=jwt.sign({email:email}, process.env.ACCESS_TOKEN , { expiresIn: '1h' })
-      res.send({result, token});
+      const result = await allUsersCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+      res.send({ result, token });
     })
 
     // get all user from DB and show UI
-    app.get('/allUsers', async(req,res)=>{
-      const users= await allUsersCollection.find().toArray();
+    app.get('/allUsers', async (req, res) => {
+      const users = await allUsersCollection.find().toArray();
       res.send(users);
 
     })
@@ -80,20 +81,20 @@ async function run() {
     //   res.send(result);
     // })
 
-      //============== Admin role get method =======================
-      app.get('/admin/:email',async(req,res)=>{
-        const email = req.params.email;
-        const user = await userCollection.findOne({email: email});
-        const isAdmin = user.userRole === 'Role_Admin';
-        res.send({admin: isAdmin});        
-      })
-      //============== Admin role get method =======================
-      app.get('/approve/:email',async(req,res)=>{
-        const email = req.params.email;
-        const user = await userCollection.findOne({email: email});
-        const isApprove = user.userRole === 'Role_Approve';
-        res.send({approve: isApprove});        
-      })
+    //============== Admin role get method =======================
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.userRole === 'Role_Admin';
+      res.send({ admin: isAdmin });
+    })
+    //============== Admin role get method =======================
+    app.get('/approve/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isApprove = user.userRole === 'Role_Approve';
+      res.send({ approve: isApprove });
+    })
     // ====================== \\  All User End  //===================
 
     // =========== Requisition Part ===================
@@ -102,11 +103,20 @@ async function run() {
       const result = await createRequisitionCollection.insertOne(requisition);
       res.send(result);
     })
-    app.get("/createRequisition",  async (req, res) => {
+    app.get("/createRequisition", async (req, res) => {
       const requisition = await createRequisitionCollection.find().toArray();
       res.send(requisition)
     })
 
+
+
+    // Requisition Preview   
+    app.get('/createRequisition/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const findPreviewData = await createRequisitionCollection.findOne(query);
+      res.send(findPreviewData);
+    })
 
     //============ Add Inventory =======================
     app.post('/addInventory', async (req, res) => {
@@ -115,7 +125,7 @@ async function run() {
       res.send(result);
     })
 
-    app.get("/addInventory",verifyJWT,  async (req, res) => {
+    app.get("/addInventory", async (req, res) => {
       const addInventory = await addInventoryCollection.find().toArray();
       res.send(addInventory)
     })
@@ -134,8 +144,8 @@ async function run() {
       res.send(result);
     })
 
-    app.get("/employee", async (req,res)=>{
-      const getEmployee= await employeeCollection.find().toArray();
+    app.get("/employee", async (req, res) => {
+      const getEmployee = await employeeCollection.find().toArray();
       res.send(getEmployee)
     })
 
@@ -146,8 +156,8 @@ async function run() {
       res.send(result);
     })
 
-    app.get("/user", async (req,res)=>{
-      const result= await userCollection.find().toArray();
+    app.get("/user", async (req, res) => {
+      const result = await userCollection.find().toArray();
       res.send(result)
     })
 
@@ -172,9 +182,9 @@ async function run() {
     })
 
     // user update data show in the from field 
-    app.get('/user/:id', async(req, res) =>{
+    app.get('/user/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) }
       const result = await userCollection.findOne(query);
       res.send(result);
     })
@@ -223,7 +233,7 @@ async function run() {
       res.send(result);
     })
 
-    
+
     //====================== Department start method ======================
     //--------------- Department post method-----------------
     app.post('/department', async (req, res) => {
@@ -246,12 +256,12 @@ async function run() {
     })
 
     //   ----------------------Department Update data show method-------------
-  app.get('/department/:id', async(req, res) =>{
-    const id = req.params.id;
-    const query = {_id: ObjectId(id)}
-    const result = await departmentCollection.findOne(query);
-    res.send(result);
-  })
+    app.get('/department/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const result = await departmentCollection.findOne(query);
+      res.send(result);
+    })
 
     //--------------- Department get method-----------------
     app.get('/department', async (req, res) => {
@@ -268,7 +278,7 @@ async function run() {
     })
 
 
-   
+
 
     //====================== Designation start method ======================
     //--------------- Designation post method-----------------
@@ -310,7 +320,7 @@ async function run() {
       res.send(result);
     })
 
-  
+
     //====================== Product key  start method ======================
     //--------------- Product key  post method--------------------  
     app.post('/productkey', async (req, res) => {
@@ -349,172 +359,172 @@ async function run() {
       const result = await productKeyCollection.updateOne(filter, updateDoc, options);
       res.send(result);
     });
-   
 
-     //====================== Budget Code  start ======================
+
+    //====================== Budget Code  start ======================
     //--------------- Budget Code post method --------------------  
-    app.post('/budgetcode', async(req, res) =>{
+    app.post('/budgetcode', async (req, res) => {
       const newBudgetCode = req.body;
       const result = await budgetCodeCollection.insertOne(newBudgetCode);
       res.send(result)
     })
     //--------------- Budget Code get method --------------------  
-    app.get('/budgetcode', async(req, res) =>{
+    app.get('/budgetcode', async (req, res) => {
       const budgetcode = await budgetCodeCollection.find().toArray();
       res.send(budgetcode);
     })
     //--------------- Budget Code update method --------------------  
-    app.put('/budgetcode/:id', async(req, res) =>{
+    app.put('/budgetcode/:id', async (req, res) => {
       const id = req.params.id;
       const budgetCode = req.body;
-      const filter = {_id: ObjectId(id)};
-      const options = {upsert: true};
-      const updateDoc={
-        $set:{
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
           budgetCode: budgetCode.budgetCode,
         }
       };
       const result = await budgetCodeCollection.updateOne(filter, updateDoc, options);
       res.send(result);
     })
-    
+
     //--------------- Budget Code delete method --------------------  
-    app.delete('/budgetcode/:id', async(req, res)=>{
+    app.delete('/budgetcode/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) }
       const budgetcode = await budgetCodeCollection.deleteOne(query);
       res.send(budgetcode);
     })
     //--------------- Budget Code update show method --------------------  
-  app.get('/budgetcode/:id', async(req, res) =>{
-    const id =req.params.id;
-    const query = {_id: ObjectId(id)}
-    const result = await budgetCodeCollection.findOne(query);
-    res.send(result);
-  })
-   
-    
+    app.get('/budgetcode/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const result = await budgetCodeCollection.findOne(query);
+      res.send(result);
+    })
+
+
     //====================== Add Product  start ====================== 
     //--------------- Product Post method--------------------  
-    app.post('/product', async(req, res)=>{
+    app.post('/product', async (req, res) => {
       const newProduct = req.body;
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     })
     //--------------- Product Get method--------------------  
-    app.get('/product', async(req,res) =>{
+    app.get('/product', async (req, res) => {
       const product = await productCollection.find().toArray();
       res.send(product);
     })
     //--------------- Product Delete method-------------------- 
-    app.delete('/product/:id', async(req, res)=>{
+    app.delete('/product/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) }
       const result = await productCollection.deleteOne(query);
       res.send(result);
-    }) 
+    })
     //--------------- Product Update/ put method--------------------  
-      app.put('/product/:id', async(req, res) =>{
-        const id = req.params.id;
-        const product = req.body;
-        const filter = {_id: ObjectId(id)};
-        const options = {upsert: true}
-        const updateDoc = {
-          $set : product,
-        }
-        const result = await productCollection.updateOne(filter, updateDoc, options);
-        res.send(result);
-      })
-    //--------------- Product Update data show  method--------------
-    app.get('/product/:id', async(req, res) =>{
+    app.put('/product/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const product = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: product,
+      }
+      const result = await productCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    })
+    //--------------- Product Update data show  method--------------
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
       const result = await productCollection.findOne(query);
       res.send(result);
     })
 
-   
+
     //====================== Suppler  start ====================== 
     //--------------- Suppler  Post method--------------------  
-    app.post('/supplier', async(req, res)=>{
+    app.post('/supplier', async (req, res) => {
       const newSupplier = req.body;
       const result = await supplierCollection.insertOne(newSupplier);
       res.send(result);
     })
     //--------------- Suppler  Get method--------------------  
-    app.get('/supplier', async(req,res) =>{
+    app.get('/supplier', async (req, res) => {
       const supplier = await supplierCollection.find().toArray();
       res.send(supplier);
     })
     //--------------- Suppler Delete method-------------------- 
-    app.delete('/supplier/:id', async(req, res)=>{
+    app.delete('/supplier/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) }
       const result = await supplierCollection.deleteOne(query);
       res.send(result);
-    }) 
+    })
     //---------------Suppler  Update/ put method--------------------  
-      app.put('/supplier/:id', async(req, res) =>{
-        const id = req.params.id;
-        const supplier = req.body;
-        const filter = {_id: ObjectId(id)};
-        const options = {upsert: true}
-        const updateDoc = {
-          $set : supplier,
-        }
-        const result = await supplierCollection.updateOne(filter, updateDoc, options);
-        res.send(result);
-      })
-    //--------------- Suppler  Update data show  method--------------
-    app.get('/supplier/:id', async(req, res) =>{
+    app.put('/supplier/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const supplier = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: supplier,
+      }
+      const result = await supplierCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    })
+    //--------------- Suppler  Update data show  method--------------
+    app.get('/supplier/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
       const result = await supplierCollection.findOne(query);
       res.send(result);
     })
 
-   
+
     //====================== Stock Adjust start ======================= 
     //--------------- Stock Adjust  Post method--------------------  
-    app.post('/stockadjust', async(req, res)=>{
+    app.post('/stockadjust', async (req, res) => {
       const newSupplier = req.body;
       const result = await stockAdjustCollection.insertOne(newSupplier);
       res.send(result);
     })
     //--------------- Stock Adjust  Get method--------------------  
-    app.get('/stockadjust', async(req,res) =>{
+    app.get('/stockadjust', async (req, res) => {
       const stockadjust = await stockAdjustCollection.find().toArray();
       res.send(stockadjust);
     })
     //--------------- Stock Adjust Delete method-------------------- 
-    app.delete('/stockadjust/:id', async(req, res)=>{
+    app.delete('/stockadjust/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) }
       const result = await stockAdjustCollection.deleteOne(query);
       res.send(result);
-    }) 
+    })
     //---------------Stock Adjust  Update/ put method--------------------  
-      app.put('/stockadjust/:id', async(req, res) =>{
-        const id = req.params.id;
-        const stockadjust = req.body;
-        const filter = {_id: ObjectId(id)};
-        const options = {upsert: true}
-        const updateDoc = {
-          $set : stockadjust,
-        }
-        const result = await stockAdjustCollection.updateOne(filter, updateDoc, options);
-        res.send(result);
-      })
-    //--------------- Stock Adjust  Update data show  method--------------
-    app.get('/stockadjust/:id', async(req, res) =>{
+    app.put('/stockadjust/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const stockadjust = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: stockadjust,
+      }
+      const result = await stockAdjustCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    })
+    //--------------- Stock Adjust  Update data show  method--------------
+    app.get('/stockadjust/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
       const result = await stockAdjustCollection.findOne(query);
       res.send(result);
     })
 
-   
-   
+
+
 
 
   }
